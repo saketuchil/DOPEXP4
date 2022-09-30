346e6a270bcaf6e29bac67d6913facb75aec45bf
Saket Uchil <43895539+saketuchil@users.noreply.github.com>
2022-09-30 02:38:22 -0700

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -5,10 +5,8 @@ class Multiplication
    {
       int p, q, r;
       System.out.println("Enter two integers for multiplication: ");
-      Scanner sc = new Scanner(System.in);   //System.in An InputStream which is typically connected to keyboard input of console programs.
-      p = sc.nextInt();    //scan data for input of integer value 
-      q = sc.nextInt();
-      r = p * q;
-      System.out.println("Multiplication of the integers = "+r);
+      //System.in An InputStream which is typically connected to keyboard input of console programs.
+    
+      System.out.println("Multiplication of the integers = ");
    }
 }
